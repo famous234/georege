59a0054f38022172d8f2f534d11b7e9a689fb6bb
george <kayceemosley7@gmail.com>
2023-01-12 07:12:26 -0800

    version2

--- deploy.py
+++ deploy.py
@@ -1,5 +1,6 @@
-deploying a python web application
-echo "hello world"
+# deploying a python web application
+#echo "hello world"
 #= shell scripts
-python['hello world']
+#python['hello world']
 #=python script
+print['helllo world'] 
